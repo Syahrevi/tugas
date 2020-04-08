d40db489a40acece30595f18aa758680daa497dd
Muhammad Syahrevi <55977012+lizardhazard@users.noreply.github.com>
2020-04-08 13:06:46 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body> 
-  
 <?php
-echo "Nama: Muhammad Syahrevi <br>";
-echo "Kelas: 11 RPL 2 <br>";
-echo "Alamat: Depok.Jawa Barat <br>";
+echo "Nama: Muhammad Syahrevi";
+echo "Kelas: 11 RPL 2 ";
+echo "Alamat: Depok.Jawa Barat ";
 ?>
-  
-  </body>
-</html>
