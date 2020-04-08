18dfd825a9c9eb58075958a971492e063b63f2ee
Muhammad Syahrevi <55977012+lizardhazard@users.noreply.github.com>
2020-04-08 13:00:44 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,1 +1,12 @@
-echo Heck
+<!DOCTYPE html>
+<html>
+<body> 
+  
+<?php
+echo "Nama: Muhammad Syahrevi <br>";
+echo "Kelas: 11 RPL 2 <br>";
+echo "Alamat: Depok.Jawa Barat <br>";
+?>
+  
+  </body>
+</html>
